6eebf44314ef8419876223e42b9ae767bebf5a6e
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-06-27 09:21:03 -0300

    test(characters/search): Testes search

--- src/components/search/index.spec.tsx
+++ src/components/search/index.spec.tsx
@@ -1,7 +1,13 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render } from "@testing-library/react";
 import { Search } from "./index";
+import { act } from "react-dom/test-utils";
 
 describe("<Search/>", () => {
+  jest.useFakeTimers(); // Usar fake timers antes dos testes
+  afterEach(() => {
+    jest.clearAllTimers(); // Limpar todos os timers
+  });
+
   it("Deveria renderizar na tela o componente Search com um Input", () => {
     const setValueAfterWrite = jest.fn();
     const screen = render(<Search setValueAfterWrite={setValueAfterWrite} />);
@@ -11,7 +17,37 @@ describe("<Search/>", () => {
 
     expect(screen.container).toMatchSnapshot();
   });
-  it.todo("Deveria ser possível buscar");
-  it.todo("Deveria ser possível buscar de forma Debounce");
+  it("Deveria ser possível buscar", () => {
+    const setValueAfterWrite = jest.fn();
+    const screen = render(<Search setValueAfterWrite={setValueAfterWrite} />);
+
+    const valueSearch = "teste";
+    const searchInput = screen.getByTestId("id-search-characters");
+
+    fireEvent.change(searchInput, {
+      target: { value: valueSearch },
+    });
+
+    expect(searchInput).toHaveValue(valueSearch);
+    expect(screen.container).toMatchSnapshot();
+  });
+  it("Deveria ser possível buscar de forma Debounce", () => {
+    const valueAfterWrite = jest.fn();
+    const valueSearch = "teste";
+
+    const screen = render(<Search setValueAfterWrite={valueAfterWrite} />);
+
+    const searchInput = screen.getByTestId("id-search-characters");
+
+    fireEvent.change(searchInput, {
+      target: { value: valueSearch },
+    });
+
+    act(() => {
+      jest.runAllTimers();
+    });
+    expect(searchInput).toHaveValue(valueSearch);
+    expect(valueAfterWrite).toHaveBeenCalledWith(valueSearch);
+    expect(screen.container).toMatchSnapshot();
+  });
 });
- 
