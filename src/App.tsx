959e8f83094714890b12e5504338b1549b5051d3
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-06-27 09:21:32 -0300

    fix(router): correção componente roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Characters } from "./pages/characters";
+import { Character } from "./pages/character";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Characters />} />
-        <Route path="characters/:id" element={<Characters />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="characters/:id"  element={<Character />} />
       </Routes>
     </BrowserRouter>
   );
