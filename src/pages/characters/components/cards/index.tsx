c23f9cfa7e96180628fe69c6d90a7127c093c9d2
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-06-28 09:19:09 -0300

    feat(characters/cards): adição paginação

--- src/pages/characters/components/cards/index.tsx
+++ src/pages/characters/components/cards/index.tsx
@@ -1,16 +1,36 @@
 import { ICharacters } from "@/interfaces/characters";
 import { Card } from "../card";
 
-import { StyledContainerCards } from "./styles";
+import {
+  CardsContainer,
+  CardsContainerPagination,
+  CardsButtonAddPage,
+} from "./styles";
 
-export const Cards: React.FC<ICharacters> = ({ data: { results } }) => {
+export const Cards: React.FC<ICharacters> = ({
+  data: { results: characters },
+  setPagination,
+  pagination,
+}) => {
   return (
-    <StyledContainerCards data-testid="id-cards-container">
-      {results.map((character, index) => (
-        <div data-testid="id-cards-content" key={index}>
-          <Card character={character}  data-testid="teste" />
-        </div>
-      ))}
-    </StyledContainerCards>
+    <>
+      <CardsContainer data-testid="id-cards-container">
+        {characters.map((character, index) => (
+          <div data-testid="id-cards-content" key={index}>
+            <Card character={character} data-testid="teste" />
+          </div>
+        ))}
+      </CardsContainer>
+      <CardsContainerPagination>
+        {pagination > 3 && (
+          <CardsButtonAddPage onClick={() => setPagination((prev) => prev - 3)}>
+            Remover
+          </CardsButtonAddPage>
+        )}
+        <CardsButtonAddPage onClick={() => setPagination((prev) => prev + 3)}>
+          Adicionar Mais
+        </CardsButtonAddPage>
+      </CardsContainerPagination>
+    </>
   );
 };
--- src/pages/characters/components/cards/styles.ts
+++ src/pages/characters/components/cards/styles.ts
@@ -1,11 +1,27 @@
 import { styled } from "styled-components";
 
-const StyledContainerCards = styled.section`
+const CardsContainer = styled.section`
   display: grid;
   justify-items: center;
   margin: 0 auto;
   grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
-  grid-gap: 70px 10px;
+  grid-gap: 20px 10px;
 `;
 
-export { StyledContainerCards };
+const CardsContainerPagination = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 100px;
+  background-color: #242424;
+`;
+
+const CardsButtonAddPage = styled.button`
+  border: none;
+  font-size: 1.5rem;
+  cursor: pointer;
+  background-color: #242424;
+`;
+
+export { CardsContainer, CardsContainerPagination, CardsButtonAddPage };
