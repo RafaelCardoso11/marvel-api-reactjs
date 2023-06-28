ad02dce36f17dbb40e609d7def61c944f735e0dc
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-06-28 13:43:50 -0300

    test(routers): path por parametro + config watch testes

--- src/tests/routers/index.tsx
+++ src/tests/routers/index.tsx
@@ -4,14 +4,16 @@ import { BrowserRouter, Route, Routes, RouteProps } from "react-router-dom";
 function RoutersContextTesting({
   children,
   routes = [],
+  router = { path: "/" },
 }: {
   children: ReactNode;
   routes?: React.ReactElement<RouteProps>[];
+  router?: { path: string };
 }) {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>{children}</>} />
+        <Route path={router.path} element={children} />
         {routes.map((route: React.ReactElement) => route)}
       </Routes>
     </BrowserRouter>
