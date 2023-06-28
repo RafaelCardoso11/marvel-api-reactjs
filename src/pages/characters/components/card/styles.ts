476f48526a9b9cabbef93614d7a11604d1f85d95
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-06-28 09:21:34 -0300

    styles(card): correção tamanhos imagem do card.

--- src/pages/characters/components/card/styles.ts
+++ src/pages/characters/components/card/styles.ts
@@ -2,10 +2,11 @@ import { styled } from "styled-components";
 
 const StyledContainerCard = styled.div`
   text-align: center;
+  cursor: pointer;
 `;
 const StyledImg = styled.img`
-  width: 100%;
-  height: 100%;
+  max-width: 350px;
+  height: 400px;
 `;
 
 const StyledTypographName = styled.h2`
