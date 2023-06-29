996046778d612ee76cec0d0a5b343636ff3026e4
ANDRE RAFAEL CARDOSO REIS <rafabatera-8@hotmail.com>
2023-06-29 12:54:22 -0300

    feat(App): Roteamento para o SearchCharacters ser a home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Characters } from "./pages/characters";
 import { Character } from "./pages/character";
+import { SearchCharacters } from "./pages/searchCharacters";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<SearchCharacters />} />
         <Route path="/characters" element={<Characters />} />
         <Route path="characters/:id"  element={<Character />} />
+        <Route path="*" element={<SearchCharacters />} />
       </Routes>
     </BrowserRouter>
   );
